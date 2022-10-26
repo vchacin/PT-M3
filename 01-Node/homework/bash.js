83f91575a175449fa97199e96154e4d8ce4561a9
vchacin <arq.vchacin@gmail.com>
2022-10-25 23:10:23 -0300

    Homework primera parte

---
+++ 01-Node/homework/bash.js
@@ -0,0 +1,36 @@
+//   console.log(process)
+//   console.log(Object.keys(process))
+  
+  /*
+  // Output un prompt
+  process.stdout.write('prompt > ');
+  // El evento stdin 'data' se dispara cuando el user escribe una línea
+  process.stdin.on('data', function (data) {
+    var cmd = data.toString().trim(); // remueve la nueva línea
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  });
+  */
+
+
+const commands = require('./commands/index.js');
+
+/*
+const cmd = 'pwd';
+commands[cmd]() // la función dentro de la propiedad pwd
+*/
+
+// Output un prompt
+process.stdout.write('prompt > ');
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+process.stdin.on('data', function (data) {
+  var args = data.toString().trim().split(' '); // remueve la nueva línea
+  var cmd = args.shift();
+
+  if(commands.hasOwnProperty(cmd)) {
+    commands[cmd](args)
+  } else {
+    process.stdout.write('Command not found');
+  }
+  process.stdout.write('\nprompt > ');
+});
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,22 @@
-
+var fs = require('fs');
 
 module.exports = {
-
+    pwd: function() { 
+        process.stdout.write(Date()); 
+    },
+    date: function() {
+        process.stdout.write(process.cwd());
+    },
+    ls: function() {
+        fs.readdir('.', function(err, files) {
+            if (err) throw err;
+            files.forEach(function(file) {
+              process.stdout.write(file.toString() + "\n");
+            })
+            process.stdout.write("prompt > ");
+        });
+    },
+    echo: function(args){
+        process.stdout.write(args.join(' '));
+    }
 }
