fabdcd6e4d38aaf6d5c990e7ee185eaed95596c8
vchacin <arq.vchacin@gmail.com>
2022-11-22 22:55:38 -0300

    Homework Generator func lista

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,18 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+  let i = 1;
+  while( max ? i < max : true ){
+    if (i % 3 == 0 && i % 5 == 0){
+      yield 'Fizz Buzz'
+    } else if (i % 3 == 0){
+      yield 'Fizz'
+    } else if (i % 5 == 0){
+      yield 'Buzz'
+    } else {
+      yield 1
+    }
+    i++
+  }
 }
 
 module.exports = fizzBuzzGenerator;
